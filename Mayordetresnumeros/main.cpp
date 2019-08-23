5e04d6ae0d32608b6639171c4acea108145247f9
Solcito25 <solmorelia25@gmail.com>
2019-08-22 21:25:57 -0500

    proyecto

--- Mayordetresnumeros/main.cpp
+++ Mayordetresnumeros/main.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
+ #include<iostream>
+ using namespace std;
+ int main (){
+ float a,b,c,d;
+        cout << "Ingrese el primer numero: " << endl;
+        cin>>a;
+        cout << "Ingrese el segundo numero: " << endl;
+        cin>>b;
+        cout << "Ingrese el tercer numero: " << endl;
+        cin>>c;
+        if(a>b)
+            d=a;
+        else
+            d=b;
+        if(d>c)
+            cout << "El numero mayor es " <<d<< endl;
+        else
+            cout << "El numero mayor es "<<c<< endl;
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
+return 0;
+ }
