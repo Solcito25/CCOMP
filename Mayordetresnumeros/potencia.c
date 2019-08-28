965c1de5cb923317754790d3c659f43fc9646b96
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2019-08-28 09:56:58 -0500

    clase

---
+++ Mayordetresnumeros/potencia.c
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ int main(){
+ cin>>a;
+ float c;
+ bool d = true;
+ while(d== true){
+    c=a/2
+    if(c-a != 0.0)
+        d
+        cout<<"No es potencia de dos"
+    else if (c==1)
+        cout<<"No es potencia de dos"
+    else
+        d=true
+
+
+ }
+ return 0;
+
+
+ }
---
+++ Mayordetresnumeros/potencia.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+ /*int main(){
+  int a;
+  int e=1;
+
+  cin>>a;
+
+ while(c<a){
+    e=e*2;
+    e=e+1;
+
+
+ }
+ if (c == a)
+        cout<<"Es potencia de dos";
+ else
+        cout<<"No es potencia de dos";
+ return 0;
+
+
+ }*/
+ int main(){
+ int a;
+ float c;
+ float b=2;
+ cin>>a;
+  while(b<a)
+    c = a%b
+    b=b+1
+
+
+
+
+ }
