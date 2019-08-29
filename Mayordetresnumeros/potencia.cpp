7575feb4f89c1a38e1f08e59c97a98966c050f50
Solcito25 <solmorelia25@gmail.com>
2019-08-28 21:47:55 -0500

    finish

--- Mayordetresnumeros/potencia.cpp
+++ Mayordetresnumeros/potencia.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
-/*int main(){
+/*
+//Numero potencia de dos
+int main(){
   int a;
   int e=1;
 
@@ -26,7 +28,8 @@ using namespace std;
     c = a%b
     b=b+1
 */
-
+/*
+//Numeros primos while
 int main (){
 	int a,d,c=0,e=0;
 	int b=1;
@@ -50,9 +53,11 @@ int main (){
     else
         cout<<"no es primo"<<endl;
 
-}
+}*/
 
-/*int main(){
+/*
+// numeros primos for
+int main(){
 int a,d,c=0,e=0;
 	int b=1;
     cin>>a;
@@ -74,4 +79,101 @@ int a,d,c=0,e=0;
         cout<<"es primo"<<endl;
  else
         cout<<"no es primo"<<endl;
+}*/
+
+/*
+//Numeros primos menores de 100 while
+int main (){
+int a=2,d,c=0,e=0,b=1;
+
+    while (a <100){
+        while(b<a){
+            d=a%b;
+            b=b+1;
+              if (d!= 0){
+                c=c+1;
+              }
+              if (d==0) {
+                e=e+1;
+
+            }
+
+        }
+
+        if (c==a-2)
+            cout<<a<<endl;
+
+
+        a=a+1;
+        c=0;
+        b=1;
+
+         }
+
+
+
+}*/
+
+// Numeros primos menores de 100 for
+/*int main(){
+int a=1,d,c=0,e=0;
+	int b=1;
+
+
+ for(a;a<100;a=a+1){
+  for(b;b<a;b=b+1){
+        d=a%b;
+
+        //cout<<"modulo"<<d<<endl;
+        if (d !=0){
+           c=c+1;
+         //  cout<<"residuo"<<d<<endl;
+        }
+        if (d==0) {
+            e=e+1;
+         //  cout<<"ceros"<<e<<endl;
+        }
+ }
+  if (c==a-2)
+        cout<<a<<endl;
+  c=0;
+  b=1;
+
+}
+} */
+// Numeros perfectos while
+/*int main(){
+ int a,b=2,c=1;
+ cin>>a;
+  while (b<a){
+    if (a%b == 0)
+        c=c+b;
+    if (a%b != 0)
+        c=c+0;
+    b=b+1;
+  }
+  if (c==a){
+    cout<<"Es un numero perfecto"<<endl;}
+  else{
+    cout<<"No es un numero perfecto"<<endl;}
+
+}*/
+// Numeros perfectos for
+/*int main(){
+ int a,b=2,c=1;
+ cin>>a;
+  for (b;b<a;b=b+1){
+    if (a%b == 0)
+        c=c+b;
+    if (a%b != 0)
+        c=c+0;
+
+  }
+  if (c==a){
+    cout<<"Es un numero perfecto"<<endl;}
+  else{
+    cout<<"No es un numero perfecto"<<endl;}
+
+
+
 }*/
