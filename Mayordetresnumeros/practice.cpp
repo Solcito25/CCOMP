92b8d198ab921908f4e02d3013a3d7206b6554a1
Solcito25 <solmorelia25@gmail.com>
2019-08-28 20:57:43 -0500

    prueba

--- Mayordetresnumeros/practice.cpp
+++ Mayordetresnumeros/practice.cpp
@@ -1,36 +1,11 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-/*int main(){
-  int a;
-  int e=1;
 
-  cin>>a;
-
- while(e<a){
-    e=e*2;
- }
- if (e == a)
-        cout<<"Es potencia de dos";
- else
-        cout<<"No es potencia de dos";
- return 0;
-
-
- }
- /*int main(){
- int a;
- float c;
- float b=2;
- cin>>a;
-  while(b<a)
-    c = a%b
-    b=b+1
-*/
-
-int main (){
+/*int main (){
 	int a,d,c=0,e=0;
 	int b=1;
     cin>>a;
+
     while(b<a){
         d=a%b;
         b=b+1;
@@ -50,14 +25,15 @@ int main (){
     else
         cout<<"no es primo"<<endl;
 
-}
+}*/
 
 /*int main(){
-int a,d,c=0,e=0;
+int a=1,d,c=0,e=0;
 	int b=1;
-    cin>>a;
 
- for(b;b<a;b=b+1){
+
+ for(a;a<100;a=a+1){
+  for(b;b<a;b=b+1){
         d=a%b;
 
         //cout<<"modulo"<<d<<endl;
@@ -69,9 +45,43 @@ int a,d,c=0,e=0;
             e=e+1;
          //  cout<<"ceros"<<e<<endl;
         }
+ }
+if (c==a-2)
+        cout<<"es primo"<<a<<endl;
+        else
+        cout<<"no es primo"<<a<<endl;
 }
- if (c==a-2)
-        cout<<"es primo"<<endl;
- else
-        cout<<"no es primo"<<endl;
+
 }*/
+
+int main (){
+int a=2,d,c=0,e=0,b=1;
+
+    while (a <100){
+        while(b<a){
+            d=a%b;
+            b=b+1;
+              if (d!= 0){
+                c=c+1;
+              }
+              if (d==0) {
+                e=e+1;
+
+            }
+
+        }
+
+        if (c==a-2)
+            cout<<a<<endl;
+
+
+        a=a+1;
+        c=0;
+
+
+    }
+
+
+
+}
+
