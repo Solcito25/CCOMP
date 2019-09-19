65da8b0bfa08201f7296f0396381530af383e306
Solcito25 <solmorelia25@gmail.com>
2019-09-18 22:38:21 -0500

    ok

---
+++ examen/examen5/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a;
+    long long b=2;
+    cin>>a;
+    for (b;a>1;b++){
+    while(a%b==0){
+        a=a/b;
+        cout<<b<<endl;
+
+    }
+
+}
+
+
+
+
+}
