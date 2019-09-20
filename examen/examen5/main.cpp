3a90ded9f3731efbd9013475e9a112f4ca76102c
Solcito25 <solmorelia25@gmail.com>
2019-09-19 20:45:54 -0500

    oki

--- examen/examen5/main.cpp
+++ examen/examen5/main.cpp
@@ -4,15 +4,20 @@ using namespace std;
 
 int main()
 {
-    long long a;
+    long long a,c;
     long long b=2;
     cin>>a;
     for (b;a>1;b++){
     while(a%b==0){
         a=a/b;
-        cout<<b<<endl;
+        c=b;
+        //cout<<b<<endl;
 
     }
+
+}
+cout<<c<<endl;
+}
 
 }
 
